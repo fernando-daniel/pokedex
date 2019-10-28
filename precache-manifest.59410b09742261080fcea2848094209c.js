1259f4f0c1e6cee67c04ce7519cb4a93353c1e27
fernandodcastanon <fernandodaniel777castanon@gmail.com>
2019-10-27 18:23:56 -0700

    Updates

---
+++ precache-manifest.59410b09742261080fcea2848094209c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4e62818a02f946af39915d021d83c9a9",
+    "url": "/pokedex/index.html"
+  },
+  {
+    "revision": "e7d31da180e441ac77f9",
+    "url": "/pokedex/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "ae98d2123655eb133635",
+    "url": "/pokedex/static/css/main.4e125e06.chunk.css"
+  },
+  {
+    "revision": "e7d31da180e441ac77f9",
+    "url": "/pokedex/static/js/2.08ac4dcf.chunk.js"
+  },
+  {
+    "revision": "ae98d2123655eb133635",
+    "url": "/pokedex/static/js/main.166df9c6.chunk.js"
+  },
+  {
+    "revision": "81a51162f9e9c7056244",
+    "url": "/pokedex/static/js/runtime-main.831d9d7f.js"
+  }
+]);
